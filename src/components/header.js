9cd22cd95051f6a061a9abffb0adf80571329a7d
Supasate Choochaisri <supasate.c@gmail.com>
2016-05-11 19:12:41 +0700

    Add action and reducer for authentication

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class Header extends Component {
+class Header extends Component {
   authButton() {
-    return <button>Sign In</button>;
+    if (this.props.authenticated) {
+      return <button onClick={() => this.props.authenticate(false)}>Sign Out</button>;
+    }
+    return <button onClick={() => this.props.authenticate(true)}>Sign In</button>;
   }
 
   render() {
@@ -24,3 +29,9 @@ export default class Header extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { authenticated: state.authenticated };
+}
+
+export default connect(mapStateToProps, actions)(Header);
